45383c018032fc1d9f355779b90b13e88362255a
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-09-30 21:59:02 +0530

    DSA

---
+++ Array/Declaration_initialisation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int A[4]={1,3,4,5};
+  for(int i=0;i<4;i++)
+  {
+  //  cout<<*(A+3)<<" ";
+  //  cout<<i[A]<<" ";
+  //  cout<<A[i]<<" ";  
+  }
+  return 0;
+}
