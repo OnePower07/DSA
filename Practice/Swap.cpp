e8d28c87774013c4c63fadcab9ba33eb5b4fd06d
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-11-10 10:00:46 +0530

    DSA

---
+++ Practice/Swap.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a=10,b=12;
+  // int temp;
+  // temp=a;
+  // a=b;
+  // b=temp;
+/*Another way of swapping*/
+  // a=a+b;
+  // b=a-b;
+  // a=a-b;
+/*Another way of swapping*/
+  // a=a*b;
+  // b=a/b;
+  // a=a/b;
+  cout<<a<<" "<<b;
+  return 0;
+}
